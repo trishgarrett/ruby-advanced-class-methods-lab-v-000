603746d5e9bf8469d16582c11cb8fd707d518917
Trisha Garrett <trisha.garrett@gmail.com>
2017-12-28 18:00:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,9 +46,9 @@ class Song
     @@all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(new_from_filename)
+  def self.new_from_filename(name)
     song = self.new
-    song.name=(new_from_filename)
+    song.name=(name)
     song
   end
 end
